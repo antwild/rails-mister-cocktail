28f7bacf0f51fb051dae3c96c3980d53b8764af5
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-05-15 14:10:33 +0100

    downloaded Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 require 'json'
 require 'open-uri'
 
+puts "Deleting old seed"
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
 response = JSON.parse(open(url).read)
 
+puts "Seeding ingredients"
 response["drinks"].each do |drink|
   Ingredient.create(name: drink["strIngredient1"])
 end
+
+puts "Done"
